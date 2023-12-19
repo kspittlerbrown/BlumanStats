70c196f99b50a0c70a87e525d3e2b88febd67081
kspittlerbrown <153403372+kspittlerbrown@users.noreply.github.com>
2023-12-18 19:12:07 -0600

    Update Bluman_Q1_Q3.R

--- BlumanStats/R/Bluman_Q1_Q3.R
+++ BlumanStats/R/Bluman_Q1_Q3.R
@@ -1,21 +1,21 @@
-Bluman_Q1_Q3 <-
-function(data) 
-{
+#Bluman_Q1_Q3 <-
+#function(data) 
+#{
 
   # Sort the data in ascending order
-  sorted_data <- sort(data)
+ # sorted_data <- sort(data)
 
   # Calculate the median (Q2)
-  median_value <- median(sorted_data)
+  #median_value <- median(sorted_data)
 
   # Split the data into lower and upper halves
-  lower_half <- sorted_data[sorted_data < median_value]
-  upper_half <- sorted_data[sorted_data > median_value]
+  #lower_half <- sorted_data[sorted_data < median_value]
+  #upper_half <- sorted_data[sorted_data > median_value]
 
   # Calculate Q1 and Q3 as the medians of the lower and upper halves
-  Q1 <- median(lower_half)
-  Q3 <- median(upper_half) 
+  #Q1 <- median(lower_half)
+  #Q3 <- median(upper_half) 
 
   # Return the results
-  return(list(Q1 = Q1, Q3 = Q3))
-}
+  #return(list(Q1 = Q1, Q3 = Q3))
+#}
