a2e665297e1eb87390f367e4ee645b5e373216ee
kspittlerbrown <153403372+kspittlerbrown@users.noreply.github.com>
2023-12-20 11:38:36 -0600

    Update mode_fcn.R

--- BlumanStats/R/mode_fcn.R
+++ BlumanStats/R/mode_fcn.R
@@ -1,9 +1,14 @@
-mode_fcn <-
-function(data) {
+#Calculate the mode
+mode_fcn <- function(data) {
   unique_values <- unique(data)
   counts <- table(match(data, unique_values))
   max_count <- max(counts)
-  modes1 <- unique_values[counts == max_count]
-  mode_counts <- data.frame(Value = modes1, Frequency = max_count)
-  return(mode_counts)
+  
+  if (max_count == 1) {
+    cat("No mode\n")
+  } else {
+    modes1 <- unique_values[counts == max_count]
+    mode_counts <- data.frame(Value = modes1, Frequency = max_count)
+    return(mode_counts)
+  }
 }
